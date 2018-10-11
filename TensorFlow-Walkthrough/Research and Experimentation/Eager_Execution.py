1d444853ea24b5dde3b5be1463e81e248953c571
Luke Lorenzini <luke.lorenzini@gmail.com>
2018-10-11 11:23:04 +0200

    Add eager execution

---
+++ TensorFlow-Walkthrough/Research and Experimentation/Eager_Execution.py
@@ -0,0 +1,84 @@
+#https://www.tensorflow.org/tutorials/eager/eager_basics
+
+import tensorflow as tf
+tf.enable_eager_execution()
+
+print(tf.add(1, 2))
+print(tf.add([1, 2], [3, 4]))
+print(tf.square(5))
+print(tf.reduce_sum([1, 2, 3]))
+print(tf.encode_base64("hello world"))
+
+# Operator overloading is also supported
+print(tf.square(2) + tf.square(3))
+
+x = tf.matmul([[1]], [[2, 3]])
+print(x.shape)
+print(x.dtype)
+
+import numpy as np
+
+ndarray = np.ones([3, 3])
+
+print("TensorFlow operations convert numpy arrays to Tensors automatically")
+tensor = tf.multiply(ndarray, 42)
+print(tensor)
+
+print("And NumPy operations convert Tensors to numpy arrays automatically")
+print(np.add(tensor, 1))
+
+print("The .numpy() method explicitly converts a Tensor to a numpy array")
+print(tensor.numpy())
+
+x = tf.random_uniform([3, 3])
+
+print("Is there a GPU available: ")
+print(tf.test.is_gpu_available())
+
+print("Is the Tensor on GPU #0: ")
+print(x.device.endswith('GPU:0'))
+
+
+def time_matmul(x):
+    #%timeit tf.matmul(x, x)
+    tf.matmul(x, x)
+
+# Force execution on CPU
+print("On CPU: ")
+with tf.device("CPU:0"):
+    x = tf.random_uniform([1000, 1000])
+    assert x.device.endswith("CPU:0")
+    time_matmul(x)
+
+# Force execution on GPU #0 if available
+if tf.test.is_gpu_available():
+    with tf.device("GPU:0"): # Or GPU:1 for the 2nd GPU, GPU:2 for the 3rd etc.
+        x = tf.random_uniform([1000, 1000])
+        assert x.device.endswith("GPU:0")
+        time_matmul(x)
+
+ds_tensors = tf.data.Dataset.from_tensor_slices([1, 2, 3, 4, 5, 6])
+
+# Create a CSV file
+import tempfile
+_, filename = tempfile.mkstemp()
+
+with open(filename, 'w') as f:
+    f.write("""Line 1
+    Line 2
+    Line 3
+    """)
+
+ds_file = tf.data.TextLineDataset(filename)
+
+ds_tensors = ds_tensors.map(tf.square).shuffle(2).batch(2)
+ds_file = ds_file.batch(2)
+
+print('Elements of Tensors:')
+for x in ds_tensors:
+    print(x)
+
+print('\nElements in ds_flie:')
+for x in ds_file:
+    print(x)
+
